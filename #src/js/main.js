2793214795e51478d4b0917b7a323f4e27d18d4d
Dmitrii Denisov <dmitriyd2009@yandex.ru>
2021-08-17 22:21:22 +0400

    адаптив меню

--- #src/js/main.js
+++ #src/js/main.js
@@ -10,6 +10,18 @@ $(function(){
     $('#fullpage').fullpage({
 		autoScrolling:true,
 		scrollHorizontally: true,
-        sectionSelector: '.page-section'
+        sectionSelector: '.page-section',
+        scrollOverflow: true,
+        anchors: ['top', 'product', 'benefits', 'specification', 'questions', 'contacts'],
+	    menu: '#header__nav'
 	});
+});
+
+$('.menu__btn').on('click', function(){
+    $('.menu__btn').toggleClass('menu__btn--active');
+    $('.menu__list').toggleClass('menu__list--active');
+});
+$('.menu__item-link').on('click', function(){
+    $('.menu__btn').removeClass('menu__btn--active');
+    $('.menu__list').removeClass('menu__list--active');
 })
